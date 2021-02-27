c6c15083b87471c7054126ff3b1a428cf7c01f61
Mike B <ozczecho@yahoo.com>
2021-02-27 13:19:36 +1100

    Bump version to .4

--- AirTouch/Aircon.cs
+++ AirTouch/Aircon.cs
@@ -32,6 +32,6 @@ namespace VzduchDotek.Net.AirTouch
             return Zones.FirstOrDefault(x=>x.Id == id);
         }
 
-        public string Version => "0.3";
+        public string Version => "0.4";
     }
 }
